338087d03b423c7520597241db118a6413b0c1da
Bruno Amaral <mail@brunoamaral.eu>
2022-05-02 20:50:22 +0100

    add template from AppSeed

--- django/admin/urls.py
+++ django/admin/urls.py
@@ -17,12 +17,21 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import include, path, re_path
-
+from bicicletas import views
 
 
 
 
 urlpatterns = [
 	path('admin/', admin.site.urls),
+    # The home page
+    path('', views.index, name='home'),
 
+    # Matches any html file
+    re_path(r'^.*\.*', views.pages, name='pages'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+
+
+
