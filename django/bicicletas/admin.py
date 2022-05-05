96827267ff7114291cdf307ca68e0845cadc4784
Bruno Amaral <mail@brunoamaral.eu>
2022-05-05 23:13:45 +0100

    make the admin look nicer

--- django/bicicletas/admin.py
+++ django/bicicletas/admin.py
@@ -3,5 +3,11 @@ from django.contrib import admin
 # Register your models here.
 from .models import *
 
-admin.site.register(Bicicleta)
+# this class define which department columns will be shown in the department admin web site.
+class BicicletaAdmin(admin.ModelAdmin):
+	# a list of displayed columns name.
+	list_display = ['nro_serie', 'marca','modelo','email']
+	search_fields = ['nro_serie', 'email', 'marca','modelo' ]
+
+admin.site.register(Bicicleta,BicicletaAdmin)
 
