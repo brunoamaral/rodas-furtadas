a595dd858d88fad08035f0479843515be2d1d06d
Bruno Amaral <mail@brunoamaral.eu>
2022-05-05 11:54:25 +0100

    validate file size

--- django/bicicletas/models.py
+++ django/bicicletas/models.py
@@ -1,7 +1,15 @@
 from django.core.files import storage
 from django.db import models
 from django.core.files.storage import FileSystemStorage
+from django.core.exceptions import ValidationError
+
 fs = FileSystemStorage(location='/media/fotos')
+
+def file_size(value): # add this to some file where you can import it from
+	limit = 1 * 1024 * 1024
+	if value.size > limit:
+			raise ValidationError('Os ficheiros não podem ser maiores do que 1 Mb.')
+
 
 # Create your models here.
 class Bicicleta(models.Model):
@@ -10,13 +18,17 @@ class Bicicleta(models.Model):
 	modelo = models.CharField(max_length=280, blank=True, null=True)
 	nro_serie = models.CharField(max_length=280, blank=True, null=True, unique=True)
 	foto = models.ImageField(storage=fs,
-	blank=False, null=False
+	blank=False, null=False,
+	validators=[file_size]
 	)
 	comprovativo = models.FileField(storage=fs,
-	blank=False, null=False
+	blank=False, null=False,
+	validators=[file_size]
 	)
 	email = models.EmailField(max_length=280, unique=False, null=False,blank=False)
 	valid = models.BooleanField(default=False, blank=False,null=False)
+
+
 
 
 
